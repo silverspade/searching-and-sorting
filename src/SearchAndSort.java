8bbbf2654e329ee382b436a35694c12de506b874
Kylie Brown <kbrown1@ucvts.tec.nj.us>
2019-02-25 14:41:37 -0500

    First Commit

--- src/SearchAndSort.java
+++ src/SearchAndSort.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SearchAndSort {
 	
 	/**
@@ -7,6 +9,38 @@ public class SearchAndSort {
 	 */
 	
 	public static void main(String[] args) {
-		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Which algorithm would you like to execute? \nOptions: bubble, selection, insertion, merge, linear, binary, quit");
+		String algorithm = in.nextLine();
+		algorithm = algorithm.toLowerCase();
+		boolean validInput = false;
+		do {
+			if (algorithm.matches("bubble") || algorithm.matches("selection") || algorithm.matches("insertion") || algorithm.matches("merge") || algorithm.matches("linear") || algorithm.matches("binary") || algorithm.matches("quit")) {
+				validInput = true;
+			} else {
+				System.out.println("Please pick one of the options \nOptions: bubble, selection, insertion, merge, linear, binary, quit");
+				algorithm = in.nextLine();
+				algorithm = algorithm.toLowerCase();
+			}
+		} while (!validInput);
+		if (!algorithm.matches("quit")) {
+			System.out.println("Which type of data would you like to use? \nOptions: integers, strings");
+			String datatype = in.nextLine();
+			datatype = datatype.toLowerCase();
+			System.out.println("How will it be stored? \nOptions: array, list");
+			String storage = in.nextLine();
+			storage = storage.toLowerCase();
+			System.out.println("Enter your data (Ex. 1,2,3,4,5): ");
+			String values = in.nextLine();
+			System.out.println("Algorithm: " + algorithm);
+			System.out.println("Datatype: " + datatype);
+			System.out.println("Storage: " + storage);
+			System.out.println("Data: " + values);
+		} else {
+			System.out.println("Thank you!");
+		}
+		in.close();
 	}
+	
+	//code
 }
