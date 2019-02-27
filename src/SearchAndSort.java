e620d2aacfdddeea2516d5d868024a78227ca444
Kylie Brown <kbrown1@ucvts.tec.nj.us>
2019-02-27 14:44:27 -0500

    Second commit

--- src/SearchAndSort.java
+++ src/SearchAndSort.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SearchAndSort {
@@ -27,15 +28,57 @@ public class SearchAndSort {
 			System.out.println("Which type of data would you like to use? \nOptions: integers, strings");
 			String datatype = in.nextLine();
 			datatype = datatype.toLowerCase();
+			validInput = false;
+			do {
+				if (datatype.matches("integers") || datatype.matches("strings")) {
+					validInput = true;
+				} else {
+					System.out.println("Please pick one of the options \nOptions: integers, strings");
+					datatype = in.nextLine();
+					datatype = datatype.toLowerCase();
+				}
+			} while (!validInput);
 			System.out.println("How will it be stored? \nOptions: array, list");
 			String storage = in.nextLine();
 			storage = storage.toLowerCase();
+			validInput = false;
+			do {
+				if (storage.matches("array") || storage.matches("list")) {
+					validInput = true;
+				} else {
+					System.out.println("Please pick one of the options \nOptions: array, list");
+					storage = in.nextLine();
+					storage = storage.toLowerCase();
+				}
+			} while (!validInput);
 			System.out.println("Enter your data (Ex. 1,2,3,4,5): ");
 			String values = in.nextLine();
-			System.out.println("Algorithm: " + algorithm);
-			System.out.println("Datatype: " + datatype);
-			System.out.println("Storage: " + storage);
-			System.out.println("Data: " + values);
+			values = values + ",";
+			int commaCount = 0;
+			for (int i = 0; i < values.length(); i++) {
+				if (values.charAt(i) == ',') {
+					commaCount++;
+				}
+			}
+			if (storage.matches("array") && datatype.matches("integers")) {
+				String[] temp = values.split(",");
+				int[] arrayValues = new int[commaCount];
+				for (int i = 0; i < temp.length; i++) {
+					arrayValues[i] = Integer.valueOf(temp[i]);
+					System.out.println(arrayValues[i]);
+				}
+				
+			} else if (storage.matches("array") && datatype.matches("strings")) {
+				String[] arrayValues = values.split(",");
+				for (int i = 0; i < arrayValues.length; i++) {
+					System.out.println(arrayValues[i]);
+				}
+			} else if (storage.matches("list") && datatype.matches("integers")) {
+				ArrayList<Integer> arrayValues = new ArrayList<Integer>();
+			} else {
+				ArrayList<String> arrayValues = new ArrayList<String>();
+
+			}
 		} else {
 			System.out.println("Thank you!");
 		}
