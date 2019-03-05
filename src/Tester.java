7a7465636333570d76e410f504e50197ed2d4775
silverspade <43217236+silverspade@users.noreply.github.com>
2019-03-04 23:52:06 -0500

    Remove tester

--- src/Tester.java
+++
@@ -1,8 +0,0 @@
-public class Tester {
-	public static void main(String[] args) {
-		String[] testing = "3,2,1,".split(",");
-		for (int i = 0; i < testing.length; i++) {
-			System.out.println(testing[i]);
-		}
-	}
-}
